f151fbeadefece97f342dc3184d7c811a8607dfc
jerrodmpackard <jerrodmpackard@gmail.com>
2024-04-12 14:06:12 -0700

    removed extra slash from url fetch

--- src/utils/DataServices.ts
+++ src/utils/DataServices.ts
@@ -1,7 +1,7 @@
 import { IUserItems, IToken, IUserData, IUserInfo } from "@/Interfaces/Interfaces"
 
 
-const url = "https://pottymapwebapi.azurewebsites.net/"
+const url = "https://pottymapwebapi.azurewebsites.net"
 
 let userData: IUserData
 
