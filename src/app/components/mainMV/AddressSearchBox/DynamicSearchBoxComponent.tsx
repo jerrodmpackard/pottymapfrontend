b10e6c04e9de6096b67c3bc1a99ccafb482ff27f
SNOWWWWWW4 <lescober7@gmail.com>
2024-05-03 14:21:09 -0700

    vercel sucks

--- src/app/components/mainMV/AddressSearchBox/DynamicSearchBoxComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
-
-const DynamicSearchBoxComponent = dynamic(() => import('./SearchBoxComponent'), {
-    ssr: false,
-})
-
-export default DynamicSearchBoxComponent
