1f93e920416a35ab13c84e1a2a75f23ff91d51fd
SNOWWWWWW4 <lescober7@gmail.com>
2024-05-10 03:27:02 -0700

    skeloton of bathroom info modal

---
+++ src/components/bathroomMV/ShowBathroom.tsx
@@ -0,0 +1,40 @@
+import { Dialog, DialogTitle, DialogContent, DialogActions, Box, Button, Alert, IconButton, Typography } from '@mui/material'
+import { Close} from '@mui/icons-material'
+import React, { useState } from 'react'
+
+const ShowBathroom = () => {
+    const [placeholder, setPlaceholder] = useState<boolean>(false)
+
+    const title = "lol"
+  return (
+    <Dialog
+    open={placeholder}
+    fullWidth={true}
+    maxWidth='md'
+    style={{zIndex: 101}}
+    >
+        <DialogTitle sx={{m:0, p:2}}>
+            {title}
+        </DialogTitle>
+        <IconButton
+          sx={{
+            position: 'absolute',
+            top: 8,
+            right: 8,
+            color: (theme) => theme.palette.grey[500]
+          }}
+          onClick={() => setPlaceholder(false)}
+        >
+          <Close />
+        </IconButton>
+        <DialogContent dividers className='h-[615px]'>
+          {/* bathroom dot info goes here */}
+        </DialogContent>
+        <DialogActions>
+            <Button>Close</Button>
+        </DialogActions> 
+    </Dialog>
+  )
+}
+
+export default ShowBathroom
