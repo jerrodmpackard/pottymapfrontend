1ccbf33c012f8871a64e76d128914261e68241a6
jerrodmpackard <jerrodmpackard@gmail.com>
2024-05-10 19:55:32 -0700

    created saveLongitude and saveLatitude useState variables to capture coordinate data. successfully captured coordinate data

--- src/components/bathroomMV/AddInfo/AddLocation.tsx
+++ src/components/bathroomMV/AddInfo/AddLocation.tsx
@@ -3,6 +3,9 @@ import { AddressAutofill, AddressMinimap, useConfirmAddress, config } from '@map
 import React, { useState, useCallback, useEffect, useRef } from 'react';
 
 interface Feature {
+  geometry: {
+    coordinates: number[]
+  }
   properties: {
     match_code: {
       confidence: string;
@@ -11,12 +14,14 @@ interface Feature {
 }
 
 
-const AddLocation = ({form, setForm, handleChange} : {form:any , setForm:any, handleChange:any}) => {
+const AddLocation = ({ form, setForm, handleChange }: { form: any, setForm: any, handleChange: any }) => {
 
   const [showMinimap, setShowMinimap] = useState<boolean>(false);
   const [feature, setFeature] = useState<Feature | null>(null);
   const [showValidationText, setShowValidationText] = useState<boolean>(false);
   const [mapToken, setMapToken] = useState<string>('');
+  const [saveLatitude, setSaveLatitude] = useState<number>(0);
+  const [saveLongitude, setSaveLongitude] = useState<number>(0);
 
   useEffect(() => {
     const accessToken: string = process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? '';
@@ -35,17 +40,25 @@ const AddLocation = ({form, setForm, handleChange} : {form:any , setForm:any, ha
     (res: any) => {
       const feature: Feature = res.features[0];
       setFeature(feature);
+      setSaveLongitude(feature.geometry.coordinates[0]);
+      setSaveLatitude(feature.geometry.coordinates[1]);
+      console.log(feature);
       setShowMinimap(true);
     },
     [setFeature, setShowMinimap]
   );
 
-  function handleSaveMarkerLocation(coordinate: any) {
-    console.log(`Marker moved to longitude ${JSON.stringify(coordinate[0])}º.`);
-    console.log(`Marker moved to latitude ${JSON.stringify(coordinate[1])}º.`);
-  }
+    function handleSaveMarkerLocation(coordinate: any) {
+      console.log(`Marker moved to longitude ${JSON.stringify(coordinate[0])}º.`);
+      console.log(`Marker moved to latitude ${JSON.stringify(coordinate[1])}º.`);
+    }
 
-  const handleSubmit = useCallback(
+    useEffect(() => {
+      console.log(`Longitude: ${saveLongitude}`);
+      console.log(`Latitude: ${saveLatitude}`);
+    }, [saveLatitude, saveLongitude])
+
+  const handleLocationSubmit = useCallback(
     async (e: React.FormEvent) => {
       e.preventDefault();
       const result = await showConfirm();
@@ -80,77 +93,77 @@ const AddLocation = ({form, setForm, handleChange} : {form:any , setForm:any, ha
   }
 
   return (
-    
-      <form ref={formRef} onSubmit={handleSubmit}>
-        <div className='grid grid-cols-2 gap-6 my-4' style={{zIndex: 2100}}>
-          <FormControl>
-            <AddressAutofill accessToken={mapToken} onRetrieve={handleRetrieve}>
-              <TextField id="mapbox-autofill" name="address"
-                label="Address"
-                variant="outlined" 
-                value={form.address}
-                margin="normal"
-                autoComplete="address-line1"
-                autoSave="true"
-                onChange={handleChange}
-              />
-            </AddressAutofill>
-          </FormControl>
 
-          <FormControl className='w-full p-2'>
-            <TextField name="city" 
+    <form ref={formRef} onSubmit={handleLocationSubmit}>
+      <div className='grid grid-cols-2 gap-6 my-4' style={{ zIndex: 2100 }}>
+        <FormControl>
+          <AddressAutofill accessToken={mapToken} onRetrieve={handleRetrieve}>
+            <TextField id="mapbox-autofill" name="address"
+              label="Address"
+              variant="outlined"
+              value={form.address}
+              margin="normal"
+              autoComplete="address-line1"
+              autoSave="true"
+              onChange={handleChange}
+            />
+          </AddressAutofill>
+        </FormControl>
+
+        <FormControl className='w-full p-2'>
+          <TextField name="city"
             label="City"
-            variant="outlined" 
+            variant="outlined"
             margin="normal"
             value={form.city}
             autoSave="true"
             autoComplete="address-level2"
             onChange={handleChange}
-            />
-          </FormControl>
+          />
+        </FormControl>
 
-          <FormControl className='w-full p-2'>
-            <TextField name="state" 
+        <FormControl className='w-full p-2'>
+          <TextField name="state"
             label="State / Region"
             variant="outlined"
-            value={form.state} 
-            autoComplete="address-level1" 
+            value={form.state}
+            autoComplete="address-level1"
             autoSave="true"
-            onChange={handleChange} 
-            />
-          </FormControl>
+            onChange={handleChange}
+          />
+        </FormControl>
 
-          <FormControl>
-            <TextField name="zipCode" 
+        <FormControl>
+          <TextField name="zipCode"
             label="Zip / Postcode"
-            variant="outlined" 
+            variant="outlined"
             value={form.zipCode}
-            autoComplete="postal-code" 
+            autoComplete="postal-code"
             autoSave="true"
-            onChange={handleChange} 
-            />
-          </FormControl>
+            onChange={handleChange}
+          />
+        </FormControl>
 
-          <div id="minimap-container" className="h-64 w-full relative my-2 col-span-2">
-            <AddressMinimap
-              canAdjustMarker={true}
-              satelliteToggle={true}
-              // @ts-ignore
-              feature={feature}
-              show={showMinimap}
-              onSaveMarkerLocation={handleSaveMarkerLocation}
-            />
-          </div>
+        <div id="minimap-container" className="h-64 w-full relative my-2 col-span-2">
+          <AddressMinimap
+            canAdjustMarker={true}
+            satelliteToggle={true}
+            // @ts-ignore
+            feature={feature}
+            show={showMinimap}
+            onSaveMarkerLocation={handleSaveMarkerLocation}
+          />
         </div>
-        
-        
-        {/* <div className={showMinimap ? 'grid grid-cols-2 gap-6 mt-10 mb-2': 'hidden' }>
+      </div>
+
+
+      {/* <div className={showMinimap ? 'grid grid-cols-2 gap-6 mt-10 mb-2': 'hidden' }>
           <Button variant="contained" type='submit' className='mr-3' onClick={hideBtns}>Add Location</Button>
           <Button variant='outlined' type="button" color='info' className="ml-3" onClick={resetForm}>Reset</Button>
         </div> */}
-        
-      </form>
-    
+
+    </form>
+
   )
 }
 
