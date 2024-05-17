95510d7aff6a91200f881d504e798428a947776a
SNOWWWWWW4 <lescober7@gmail.com>
2024-05-17 11:19:16 -0700

    deleted extra file

--- src/components/mainMV/DynamicMapViewPage.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-import React from 'react'
-
-const DynamicMapViewPage = dynamic(() => import('./MapPageComponent'), {
-  ssr:false,
-})
-
-export default DynamicMapViewPage
